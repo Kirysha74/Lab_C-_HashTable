622744c81819ebad7c3009621a8e827f1fd7415e
Kirill Trofimov <kaha2015-2016@mail.ru>
2024-04-19 10:46:13 +0400

    Финальная версия

--- test/hashtable_tests.cc
+++ test/hashtable_tests.cc
@@ -1,12 +1,43 @@
-#include <cmath>
-#include <iostream>
-#include <random>
 #include <hashtable.h>
 
 using namespace hashtable;
 
 int main() {
-	HashTable a(10);
-	cout << a.hash_function(5);
+	srand(173);
+
+	HashTable a;
+
+	for (int i = 1; i < 10; ++i) {
+		a.insert_value(rand());
+	}
+
+	HashTable b(a);
+
+	HashTable c = b;
+
+	cout << "Hash Table A" << endl;
+
+	a.print();
+
+	cout << "Hash Table B" << endl;
+
+	b.print();
+
+	cout << "Hash Table C" << endl;
+	 
+	c.print();
+
+	cout << " --- " << c.contains(2993) << endl;
+
+	cout << " --- " << c.count(3) << endl;
+
+	cout << " --- " << c.erase(32185) << endl;
+
+	c.print();
+
+
+
+	cout << task();
+
 	return 1;
 }
